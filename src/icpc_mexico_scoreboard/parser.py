1c2b2d67503cad3b3ff6ccc95ee9962e68336dc8
Moises Osorio <m@osr.io>
2023-05-05 22:31:51 -0700

    Migrate type classes to dataclasses.

--- src/icpc_mexico_scoreboard/parser.py
+++ src/icpc_mexico_scoreboard/parser.py
@@ -49,29 +49,38 @@ def _parse_boca_scoreboard(scoreboard_url: str) -> ParsedBocaScoreboard:
     scoreboard = ParsedBocaScoreboard()
     for teams_element in teams_elements:
         cell_elements = teams_element.find_all("td")
-        team = ParsedBocaScoreboardTeam()
-        team.place = int(cell_elements[0].text.strip())
-        team.user_site = cell_elements[1].text.strip()
-        team.name = cell_elements[2].text.strip()
+
+        name = cell_elements[2].text.strip()
+        # Multi-sites have duplicate teams, only parse the first one
+        if scoreboard.teams and scoreboard.teams[-1].name == name:
+            continue
+
+        place = int(cell_elements[0].text.strip())
+        user_site = cell_elements[1].text.strip()
         total_text_parts = cell_elements[-1].text.strip().split()
-        team.total_solved = int(total_text_parts[0])
-        team.total_penalty = int(total_text_parts[1][1:-1])
+        total_solved = int(total_text_parts[0])
+        total_penalty = int(total_text_parts[1][1:-1])
+        problems = []
         for idx, problem_element in enumerate(cell_elements[3:-1]):
-            problem_result = ParsedBocaScoreboardProblem(problem_names[idx])
+            tries = 0
+            solved_at = 0
+            is_solved = False
             result_element = problem_element.find("font")
             if result_element:
                 result_text_parts = result_element.text.strip().split("/")
-                problem_result.tries = int(result_text_parts[0])
+                tries = int(result_text_parts[0])
                 penalty_text = result_text_parts[1]
                 if penalty_text != "-":
-                    problem_result.solved_at = int(penalty_text)
-                    problem_result.is_solved = True
+                    solved_at = int(penalty_text)
+                    is_solved = True
 
-            team.problems.append(problem_result)
+            problem_result = ParsedBocaScoreboardProblem(
+                name=problem_names[idx], tries=tries, solved_at=solved_at, is_solved=is_solved)
+            problems.append(problem_result)
 
-        # Multi-sites have duplicate teams, only parse the first one
-        if scoreboard.teams and scoreboard.teams[-1].name == team.name:
-            continue
+        team = ParsedBocaScoreboardTeam(
+            name=name, place=place, user_site=user_site, total_solved=total_solved, total_penalty=total_penalty,
+            problems=problems)
         scoreboard.teams.append(team)
 
     return scoreboard
@@ -101,36 +110,44 @@ def _parse_animeitor_scoreboard(scoreboard_url: str) -> ParsedBocaScoreboard:
     for teams_element in teams_elements:
         if 'display:none' in teams_element.get('style', ''):
             continue
+
         team_prefix = teams_element.find(class_="run_prefix")
-        team = ParsedBocaScoreboardTeam()
-        team.place = int(team_prefix.find_all(class_="colocacao")[1].text.strip())
-        # team.user_site = None
-        team.name = team_prefix.find(class_="nomeTime").text.strip()
-        team.total_solved = int(team_prefix.find(class_="cima").text.strip())
-        team.total_penalty = int(team_prefix.find(class_="baixo").text.strip())
+        name = team_prefix.find(class_="nomeTime").text.strip()
+        # Multi-sites have duplicate teams, only parse the first one
+        if scoreboard.teams and scoreboard.teams[-1].name == name:
+            continue
+
+        place = int(team_prefix.find_all(class_="colocacao")[1].text.strip())
+        total_solved = int(team_prefix.find(class_="cima").text.strip())
+        total_penalty = int(team_prefix.find(class_="baixo").text.strip())
 
         problem_elements = teams_element.find_all(class_="cell", recursive=False)
+        problems = []
         for idx, problem_element in enumerate(problem_elements):
-            problem_result = ParsedBocaScoreboardProblem(problem_names[idx])
+            tries = 0
+            solved_at = 0
+            is_solved = False
             result_text = problem_element.text.strip()
             if result_text != "-":
                 if result_text.startswith("X"):
-                    problem_result.tries = int(result_text[2:-1])
+                    tries = int(result_text[2:-1])
                 else:
                     accepted_element = problem_element.find(class_="accept-text")
                     if not accepted_element:
                         # TODO: Get tries
                         continue
                     tries_text = accepted_element.contents[0].text.strip()
-                    problem_result.tries = 1 + int((tries_text[1:] or "0"))
-                    problem_result.solved_at = int(accepted_element.contents[2].text.strip())
-                    problem_result.is_solved = True
+                    tries = 1 + int((tries_text[1:] or "0"))
+                    solved_at = int(accepted_element.contents[2].text.strip())
+                    is_solved = True
 
-            team.problems.append(problem_result)
+            problem_result = ParsedBocaScoreboardProblem(
+                name=problem_names[idx], tries=tries, solved_at=solved_at, is_solved=is_solved)
+            problems.append(problem_result)
 
-        # Multi-sites have duplicate teams, only parse the first one
-        if scoreboard.teams and scoreboard.teams[-1].name == team.name:
-            continue
+        team = ParsedBocaScoreboardTeam(
+            name=name, place=place, user_site='', total_solved=total_solved, total_penalty=total_penalty,
+            problems=problems)
         scoreboard.teams.append(team)
 
     return scoreboard
--- src/icpc_mexico_scoreboard/types.py
+++ src/icpc_mexico_scoreboard/types.py
@@ -1,33 +1,16 @@
+from dataclasses import dataclass
 from typing import List
 
 
+@dataclass
 class ParsedBocaScoreboardProblem:
     name: str
-    tries: int = 0
-    solved_at: int = 0
-    is_solved: bool = False
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def __eq__(self, other):
-        if not isinstance(other, ParsedBocaScoreboardProblem):
-            return False
-        return (
-            self.name == other.name
-            and self.tries == other.tries
-            and self.solved_at == other.solved_at
-            and self.is_solved == other.is_solved
-        )
-
-    def __str__(self):
-        penalty_text = str(self.solved_at) if self.is_solved else "-"
-        return f"{self.name} ({self.tries}/{penalty_text})"
-
-    def __repr__(self):
-        return self.__str__()
+    tries: int
+    solved_at: int
+    is_solved: bool
 
 
+@dataclass
 class ParsedBocaScoreboardTeam:
     name: str
     place: int
@@ -36,10 +19,8 @@ class ParsedBocaScoreboardTeam:
     total_penalty: int
     problems: List[ParsedBocaScoreboardProblem]
 
-    def __init__(self):
-        self.problems = []
 
-
+@dataclass
 class ParsedBocaScoreboard:
     teams: List[ParsedBocaScoreboardTeam]
 

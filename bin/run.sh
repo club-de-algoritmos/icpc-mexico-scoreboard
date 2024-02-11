a24c305430a6096b44ee9a0464929db514cc3f62
Moises Osorio <m@osr.io>
2024-02-11 10:14:13 -0700

    Rename run script to a better name.

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@
 # Run from the root directory
 pip install -r requirements.txt &&\
   python src/manage.py migrate &&\
-  nohup python src/run.py > scoreboard.log &
+  nohup python src/run_scoreboard.py > scoreboard.log &

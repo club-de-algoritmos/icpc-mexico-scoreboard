e8385cc1581c4755bdb071426543846496bc659e
Moises Osorio <m@osr.io>
2024-03-06 22:15:06 -0700

    Improve run script.

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+set -e
+
+scoreboard_pid=$(ps -aux | grep run_scoreboard | grep -v grep | awk '{ print $2 }')
+if [ -z "$scoreboard_pid" ]; then
+  echo "scoreboard was not running";
+else
+  echo "scoreboard is running and will now be killed...";
+  kill "$scoreboard_pid"
+fi
+
+sleep 2
+
 # Run from the root directory
-pip install -r requirements.txt &&\
-  python src/manage.py migrate &&\
-  nohup python src/run_scoreboard.py > scoreboard.log &
+pip install -r requirements.txt
+python src/manage.py migrate
+nohup python src/run_scoreboard.py > scoreboard.log 2>&1 &
+
+echo "scoreboard is now running!"
+echo
